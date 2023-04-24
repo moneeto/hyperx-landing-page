d2c8fc3ea5f86b17bda295c8a2d0375a9fd5f582
moneeto <joako600@hotmail.com>
2023-04-24 02:38:51 -0300

    added background to product section

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,28 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Product.scss";
 import KeyboardImg from "../../media/hyper-alloy-fps.png";
 import KeyboardBackground from "../../media/alloy-fps-02.jpg";
 
 export const Product = () => {
+
+  const [shown, setShown] = useState(false)
+
   return (
     <section id="product-section">
-      <div className="product-img-div">
-        <img
-          className="product-background-img"
-          src={KeyboardBackground}
-          alt="HyperX Alloy FPS background"
-        />
-        <img className="product-img" src={KeyboardImg} alt="HyperX Alloy FPS" />
-      </div>
-      <div className="product-text-div">
-        <h1 className="product-title">HyperX Alloy FPS</h1>
-        <p className="product-p">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio
-          repellendus ratione voluptatem dignissimos non quo amet sequi officia,
-          aspernatur, ea, dolores enim officiis expedita placeat nemo reiciendis
-          tenetur totam ipsum!
-        </p>
-      </div>
+        <div className="product-text-div">
+          <h1 className="product-title">HyperX Alloy FPS</h1>
+          <p className="product-p">
+            Confortable and confident mechanical keyboard
+          </p>
+        </div>
+        <div className="product-img-div" onMouseEnter={() => {setShown(true)}} onMouseLeave={() => {setShown(false)}}>
+          <img
+            className="product-background-img"
+            src={KeyboardBackground}
+            alt="HyperX Alloy FPS background"
+          />
+          <a
+            href="https://www.youtube.com"
+            rel="noreferrer noopener"
+            target="_blank"
+          >
+            <img
+              className="product-img"
+              src={KeyboardImg}
+              alt="HyperX Alloy FPS"
+            />{shown ? <p className="hover-p">HyperX Alloy FPS</p> : null}
+          </a>
+        </div>
     </section>
   );
 };
